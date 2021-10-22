ca61a5f69e24339aabdd3167f6fa044ba714223b
Nicola-Perera <nicola.perera96@mail.com>
2021-10-22 23:30:05 +0200

    created player profile object to add to team members list

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+/*
+    Rappresento, utilizzando la struttura dati piú appropriata,
+     una lista di membri di un team.
+    Ciascun membro del team avrá come caratteristica name, role, image
+*/
+
+
+
+const player = {
+    nome: 'Nicola',
+    ruolo: 'portiere',
+    foto: `url(https://picsum.photos/id/1/200/300)`
+};
+
+// for (let key in player) {
+//     console.log(player[key]);
+// }
+
+const teamMemebrs = [];
+for (let i = 0; i < 11; i++) {
+    team.push(player);
+}
+// console.log(team);
+
+
+
+                        
