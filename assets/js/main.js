8b2feccd5b34deefd833b23f9dbfed0db6044908
Nicola-Perera <nicola.perera96@mail.com>
2021-10-23 00:40:58 +0200

    created a function that returns the final formation list of soccer team
    inserisco come parametro la lista dei ruoli

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,20 +7,25 @@
 const teamMembers = [];
 const members = 11;
 const playersName = ['Nicola', 'Matteo', 'Marco', 'Luca', 'Giovanni', 'Alessandro', 'Stefano', 'Andrea', 'Federico', 'Tommaso', 'Filippo', ]
+const soccer11Layout = {portiere: 1, difensore: 4, mediano: 3, attaccante: 2, punta: 1};
+
+const finalLayout = revealLayout (soccer11Layout);
+console.log(finalLayout);
 
 
-const player = {
-    nome: 'Nicola',
-    ruolo: 'portiere',
-    foto: `url(https://picsum.photos/id/1/200/300)`
-};
+//LE MIE FUNZIONI
+function revealLayout (soccer11Layout) {
+    const rolesList = [];
 
-
-
-for (let i = 0; i < members; i++) {
-    teamMembers.push(player);
+for (let key in soccer11Layout) {
+    console.log(soccer11Layout[key]);
+    const numberPerRole = soccer11Layout[key];
+    for (let i = 0; i < numberPerRole; i++) {
+        rolesList.push(`${key}`);
+    }
 }
-// console.log(teamMembers);
+return rolesList;
+}
 
 
 
